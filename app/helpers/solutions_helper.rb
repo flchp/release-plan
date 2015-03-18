23d785e41ab0b877c9f196558b1a6acef9406498
xdite <xdite@spoonrocket.com>
2015-03-18 16:50:05 -0700

    solution

--- app/helpers/solutions_helper.rb
+++ app/helpers/solutions_helper.rb
@@ -18,4 +18,16 @@ module SolutionsHelper
       link_to("Edit", edit_problem_solution_path(solution.problem_id, solution), :class => "underline")
     end
   end
+
+  def render_agree_level(comment)
+    case comment.agree_level
+    when "agree"
+      content_tag(:span, "Agree", :class => "label label-success")
+    when "object"
+      content_tag(:span, "Object", :class => "label label-warning")
+    when "against"
+      content_tag(:span, "Against", :class => "label label-danger")
+    else
+    end
+  end
 end
